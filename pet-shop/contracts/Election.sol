a3ccb1be65f800c080369c6b99c17ad1390a5479
Aaron Katzin <katzin.aaron@gmail.com>
2022-01-19 15:01:03 +0200

    Create basic election contract to test our environment

---
+++ pet-shop/contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.16;
+
+contract Election{
+    // store candidate
+    // read candidate
+    string public candidate;
+    // constructor
+
+    constructor () public{
+        candidate = "Candidate 1";
+    }
+}
