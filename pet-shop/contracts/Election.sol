977203678b9f879d89ad379e6746b62a93fd9275
Aaron Katzin <katzin.aaron@gmail.com>
2022-01-19 15:32:46 +0200

    Declare candidate struct and mapping. add 2 candidates in constructor

--- pet-shop/contracts/Election.sol
+++ pet-shop/contracts/Election.sol
@@ -1,12 +1,27 @@
-pragma solidity ^0.5.16;
+pragma solidity ^0.4.22;
 
 contract Election{
-    // store candidate
-    // read candidate
-    string public candidate;
-    // constructor
+    // model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
-    constructor () public{
-        candidate = "Candidate 1";
+    // store candidates
+    // fetch candidate
+    mapping(uint => Candidate) public candidates;
+
+    // store candidates count
+    uint public candidatesCount;
+
+    constructor() public{
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string _name) private{
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
