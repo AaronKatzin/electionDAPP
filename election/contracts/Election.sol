87e23e0fb90024d728d039b97106fa254cad0609
Aaron Katzin <katzin.aaron@gmail.com>
2022-01-22 19:20:15 +0200

    add vote functionality

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -7,7 +7,8 @@ contract Election{
         string name;
         uint voteCount;
     }
-
+    //store accounts that have voted
+    mapping(address => bool) public voters;
     // store candidates
     // fetch candidate
     mapping(uint => Candidate) public candidates;
@@ -23,5 +24,19 @@ contract Election{
     function addCandidate(string _name) private{
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        //require that they haven't voted yet
+        require(!voters[msg.sender]);
+
+        //require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        //record that voter has voted
+        voters[msg.sender] = true;
+
+        //update candidate vote count
+        candidates[_candidateId].voteCount++;
     }
 }
